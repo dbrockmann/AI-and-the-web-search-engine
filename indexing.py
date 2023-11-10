4ce0a82dd9eba61e6ee6b2c3f9d8566bcae6f2c4
dbrockmann <63858736+dbrockmann@users.noreply.github.com>
2023-11-10 01:31:53 +0100

    update to index class fix #2

--- indexing.py
+++ indexing.py
@@ -1,22 +1,28 @@
 
-def build_index(html_contents):
+class Index:
     """
-    Builds a simple dictionary based index with each word as the keys and a list of URLs that refer to pages including the word as values. Receives a dictionary with URLs as keys and HTML content as values.
-
-    Args:
-        html_contents: dictionary of URLs to HTML content
-
-    Returns:
-        dictionary of words to URLs which contain the word
+    Builds a simple dictionary based index with each word as the keys and a list of URLs that refer to pages including the word as values
     """
 
-    index = dict()
+    def __init__(self):
+        """
+        Initialize empty index
+        """
 
-    for url in html_contents:
-        # ??
-        for word in html_contents[url]:
+        self.index = dict()
+
+    def add(self, url, html_content):
+        """
+        Adds content from an URL to the index. Receives an URL as keys and HTML content.
+
+        Args:
+            url: URL to the content
+            html_content: HTML content
+        """
+
+        for word in html_content:
             
-            if word in index:
-                index[word].append(url)
+            if word in self.index:
+                self.index[word].append(url)
             else:
-                index[word] = [url]
+                self.index[word] = [url]
